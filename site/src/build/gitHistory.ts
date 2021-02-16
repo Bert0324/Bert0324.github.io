586e31a2266b4460c67440965eac069e6d17eb86
Bert <yuchenhuang0324@gmail.com>
2021-02-16 17:13:50 +0800

    feat: static site

--- site/src/build/gitHistory.ts
+++ site/src/build/gitHistory.ts
@@ -1,11 +1,10 @@
 import gitlog from 'gitlog';
 import { projectRootPath } from './config';
 
-const commits = gitlog({
-	repo: projectRootPath
-});
+const commits = gitlog({ repo: projectRootPath });
 export const getFileHistory = async (filePath: string) => {
 	const currentCommits = commits.filter(({ files }) => files.includes(filePath.replace(`${projectRootPath}/`, '')));
-	const date = currentCommits?.[0]?.authorDate;
+	const list = currentCommits?.sort(({ authorDate: authorDateA }, { authorDate: authorDateB }) => Number(new Date(authorDateB)) - Number(new Date(authorDateA)));
+	const date = list?.[0]?.authorDate;
 	return date ? new Date(date) : new Date();
 }
