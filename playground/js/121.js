5d54a0f8003839e11b530498baf63645305f7327
Bert <yuchenhuang0324@gmail.com>
2022-02-22 00:19:33 +0800

    feat: 121

--- playground/js/121.js
+++ playground/js/121.js
@@ -2,10 +2,13 @@
  * @param {number[]} prices
  * @return {number}
  */
- var maxProfit = function(prices) {
-	const dp = [];
-	prices.forEach((price, i) => {
-
-	});
-	
+var maxProfit = function (prices) {
+  let max = -Infinity;
+  let ret = 0;
+  for (let i = prices.length - 1; i >= 0; i--) {
+    const v = max - prices[i];
+    max = Math.max(max, prices[i]);
+    if (v > ret) ret = v;
+  }
+  return ret;
 };
